906238cc73b3c3ffbb225480376751abdf9215d3
Iris Daniela Classon <iris@irisclasson.com>
2018-09-30 21:22:45 +0200

    Script that checks for pending updates, and ignores Silverlight unless installed

---
+++ Check for Windows Updates.ps1
@@ -0,0 +1,31 @@
+﻿$msUpdateSession = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$env:COMPUTERNAME))
+$updates = $msUpdateSession.CreateUpdateSearcher().Search("IsInstalled=0").Updates
+
+$silverLightInstalled = Get-WindowsFeature | where { $_.Name.ToLower() -like 'silverlight' }
+
+$pendingUpdates = [System.Collections.ArrayList]@()
+
+$updates | % {
+
+    if ($_.Title.ToLower().Contains("Silverlight" -and $silverLightInstalled)){
+        $pendingUpdates.Add($_.Title)
+    }
+    else{
+        $pendingUpdates.Add($_.Title)
+    }
+}
+
+if($pendingUpdates -gt 0){
+    Write-Host "***** $($pendingsUpdates.Count) updates not installed *****" -BackgroundColor Black -ForegroundColor Yellow
+
+    $updates | % { Write-Host $_}
+}
+else{
+    Write-Host "***** No pending updates *****" -BackgroundColor Black -ForegroundColor Green
+}
+
+
+
+# Installed updates by date (descending
+# Doesn't show all updates?
+# $msUpdateSession.CreateUpdateSearcher().Search("IsInstalled=1").Updates | Sort-Object -Property LastDeploymentChangeTime -Descending | % {Write-Host "$($_.Title) $($_.LastDeploymentChangeTime)"}
