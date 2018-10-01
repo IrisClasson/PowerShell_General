1e4f9fba270f642bfb774cfc4ac8dcdbf4949463
irisclasson <iris@irisclasson.com>
2018-10-01 11:24:23 +0200

    Encrypt decrypt web config file example

---
+++ IIS/Encrypt decrypt web config file.ps1
@@ -0,0 +1,14 @@
+﻿# Copy web config to temp location so we are working with a copy!
+
+# Example
+$path = "C:\Users\$env:USERNAME\Documents\tmp"
+
+$iis =  "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe" 
+
+# Decrypt
+.$iis -pdf "appSettings" $path
+
+# Encrypt
+.$iis -prov "DataProtectionConfigurationProvider" -pef "appSettings" $path
+
+# WARNING! Never copy it back to original location unencrypted!
