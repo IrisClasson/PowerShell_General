2c69809765eda4aebbe3aacf115de173dbf057ea
irisclasson <iris@irisclasson.com>
2018-10-05 12:00:00 +0100

    Added Log Analytics REST call example using multiple resources

---
+++ Azure/Log Analytics query across resources using REST.ps1
@@ -0,0 +1,14 @@
+﻿$app1Id = "GUID"
+$app2Id = "GUID"
+
+$app1Key = "string"
+$app2Key = "string"
+
+# EXAMPLE: "X-Api-Key" = "key1:GUID1,key2:GUID2"
+$headers = @{ "X-Api-Key" = "${app1Key}:$app1Id,${app2Key}:$app2Id"; "Content-Type" = "application/json" }
+
+# EXAMPLE: "query" = "union app('GUID1').something, app('GUID2').something | limit 5"
+$query = @{"query" = "let days=1d;union app('$app1Id').exceptions,app('$app2Id').exceptions | where timestamp > ago(days)"}
+$body = ConvertTo-Json $query | % { [regex]::Unescape($_) }
+
+$result = Invoke-RestMethod "https://api.applicationinsights.io/v1/apps/$app1Id/query" -H $headers -Body $body -Method POST
